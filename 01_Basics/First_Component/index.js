df380f714d5b81b1e0a0036ff824e67ae88a5331
williamg1750 <william.guan12@gmail.com>
2021-06-25 17:32:25 -0400

    first component

---
+++ 01_Basics/First_Component/index.js
@@ -0,0 +1,25 @@
+//class base component
+// class Hello extends React.Component {
+//   render() {
+//     return(
+//   <div>
+//     <h1>Hello there!</h1>
+//     <h1>Hello there!</h1>
+//     <h1>Hello there!</h1>
+//   </div>
+// )
+//   }
+// }
+
+//function base component
+function Hello() {
+  return (
+    <div>
+      <h1>Hello there!</h1>
+      <h1>Hello there!</h1>
+      <h1>Hello there!</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
